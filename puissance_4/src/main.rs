195f02015e160c66d16714d5a5f32ff679571a25
idrissa <utileidrissa@gmail.com>
2024-09-23 00:13:09 +0200

    finished puissance4

--- puissance_4/src/main.rs
+++ puissance_4/src/main.rs
@@ -1,26 +1,21 @@
-use std::{fs::remove_dir, io, ops::{Index, Range}, process::exit};
-
 use colored::Colorize;
 
 pub struct Game {
 	grill: Vec<String>,
 }
 
+#[derive(Debug)]
 enum Direction {
-	n ,
-	s ,
-	w ,
-	e ,
-	nw ,
-	ne ,
-	sw ,
-	se ,
+	South ,
+	Est ,
+	SWest ,
+	SEst ,
 }
 
 impl Game {
 	pub fn new() -> Game
 	{
-		let mut grill = vec![
+		let grill = vec![
 			"0000000".to_string(),
 			"0000000".to_string(),
 			"0000000".to_string(),
@@ -43,7 +38,11 @@ impl Game {
 				if c == '0' {
 					print!("  | ")
 				} else {
-					print!("{} | ", c);
+					print!("{} | ", match c {
+						'r' | 'R' => c.to_string().red(),
+						'b' | 'B' => c.to_string().blue(),
+						_ => "?".white(),
+					});
 				}
 			}
 			println!("");
@@ -86,19 +85,24 @@ impl Game {
 		for r in &self.grill {
 			col = 0;
 			for c in r.chars() {
-				let directions = vec![Direction::s, Direction::n, Direction::w, Direction::e, Direction::sw, Direction::se, Direction::nw, Direction::ne];
-
+				let directions = vec![
+					Direction::South,
+					Direction::Est,
+					Direction::SWest,
+					Direction::SEst,
+					];
 				if c == '0' {
 					continue ;
 				} else {
 					for dir in &directions {
-						if self.count_aligned(c, dir, col, row) >= 3 {
-							
+						if self.count_aligned(c, dir, col, row) >= 3
+						{
 							self.hilight_winner(col, row, dir, c);
+							std::process::Command::new("clear").status().unwrap();
 							self.print_grill();
 							println!("Player {} wins !!!", match c {
-								'r' => "red".red(),
-								'b' => "blue".blue(),
+								'r' | 'R' => "red".red(),
+								'b' | 'B' => "blue".blue(),
 								_ => "?".white(),
 							});
 							return  true;
@@ -114,7 +118,7 @@ impl Game {
 	fn count_aligned(&self, player: char, dir: &Direction, col: usize, row: usize) -> usize
 	{
 		match dir {
-			Direction::s => {
+			Direction::South => {
 				if row == 0 { return 0 }
 				if player == self.grill[row - 1].chars().nth(col).unwrap() {
 					return 1 + self.count_aligned(player, dir, col, row - 1);
@@ -122,16 +126,7 @@ impl Game {
 					return 0;
 				}
 			}
-			Direction::n => {
-				if row + 1 >= self.grill.len() { return 0 }
-
-				if player == self.grill[row + 1].chars().nth(col).unwrap() {
-					return 1 + self.count_aligned(player, dir, col, row + 1);
-				} else {
-					return 0;
-				}
-			}
-			Direction::e => {
+			Direction::Est => {
 				if col + 1 >= self.grill[0].len() {
 					return 0;
 				}else if player == self.grill[row].chars().nth(col + 1).unwrap() {
@@ -140,16 +135,7 @@ impl Game {
 					return 0;
 				}
 			}
-			Direction::w => {
-				if col == 0 { return 0 }
-
-				if player == self.grill[row].chars().nth(col - 1).unwrap() {
-					return 1 + self.count_aligned(player, dir, col - 1, row);
-				} else {
-					return 0;
-				}
-			}
-			Direction::se => {
+			Direction::SEst => {
 				if col + 1 >= self.grill[0].len() || row == 0 { return 0 }
 
 				if player == self.grill[row - 1].chars().nth(col + 1).unwrap() {
@@ -158,29 +144,11 @@ impl Game {
 					return 0;
 				}
 			}
-			Direction::sw => {
+			Direction::SWest => {
 				if row == 0 || col == 0 { return 0 }
 
 				if player == self.grill[row - 1].chars().nth(col - 1).unwrap() {
 					return 1 + self.count_aligned(player, dir, col - 1, row - 1);
-				} else {
-					return 0;
-				}
-			}
-			Direction::ne => {
-				if col + 1 >= self.grill[0].len() || row + 1 >= self.grill.len() { return 0 }
-
-				if player == self.grill[row + 1].chars().nth(col + 1).unwrap() {
-					return 1 + self.count_aligned(player, dir, col + 1, row + 1);
-				} else {
-					return 0;
-				}
-			}
-			Direction::nw => {
-				if col + 1 >= self.grill[0].len() || col == 0 { return 0 }
-
-				if player == self.grill[row + 1].chars().nth(col - 1).unwrap() {
-					return 1 + self.count_aligned(player, dir, col - 1, row + 1);
 				} else {
 					return 0;
 				}
@@ -193,34 +161,22 @@ impl Game {
 		{
 			self.grill[row].remove(col);
 			self.grill[row].insert(col, player.to_ascii_uppercase());
-			match dir {
-				Direction::s => {
+			if i == 3 {
+				return ;
+			} match dir {
+				Direction::South => {
 					row -= 1;
 				}
-				Direction::n => {
-					row += 1;
-				}
-				Direction::e => {
+				Direction::Est => {
 					col += 1;
 				}
-				Direction::w => {
-					col -= 1;
-				}
-				Direction::se => {
+				Direction::SEst => {
 					row -= 1;
 					col += 1;
 				}
-				Direction::sw => {
+				Direction::SWest => {
 					row -= 1;
 					col -= 1;
-				}
-				Direction::ne => {
-					col += 1;
-					row += 1;
-				}
-				Direction::nw => {
-					col -= 1;
-					row += 1;
 				}
 			}
 		}
@@ -240,7 +196,7 @@ fn main() {
 			_ => "?".white(),
 		});
 		let mut input = String::new();
-		std::io::stdin().read_line(&mut input);
+		let _ = std::io::stdin().read_line(&mut input);
 		println!("");
 		let mut input: usize = match input.trim().parse() {
 			Ok(num) => num,
