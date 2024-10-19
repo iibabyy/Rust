9caa908f59f181ecec6e9f4e267fd87ac97a364a
idrissa <utileidrissa@gmail.com>
2024-10-19 20:24:12 +0200

    save

--- text_analyser/src/main.rs
+++ text_analyser/src/main.rs
@@ -1,5 +1,7 @@
-use std::{collections::HashMap, env, fs::*, path::Path};
 
+use std::{collections::HashMap, env, fs::*, path::Path, result};
+
+#[allow(dead_code)]
 pub struct Stats {
 	phrases: u64,
 	words: u64,
@@ -9,50 +11,72 @@ pub struct Stats {
 	use_words: String,
 }
 
+#[allow(dead_code)]
 impl Stats {
-	fn count_chars(text: String) -> usize {
-		text.chars().filter(|c| c.is_whitespace() == false).count()
+	fn count_chars(text: &String) -> usize {
+		text
+		.chars()
+		.filter(|c| c.is_whitespace() == false)
+		.count()
 	}
-	fn count_words(text: String) -> usize
-	{
-		text.split_whitespace().enumerate().count()
+	fn count_lines(text: &String) -> usize {
+		text
+		.split(|c| c == '\n')
+		.filter(|str| str.is_empty() == false)
+		.count()
 	}
-	fn count_phrases(text: String) -> usize
+	fn count_words(text: &String) -> usize
 	{
-		text.split(|c| c == '.' || c == '?' || c == '!')
+		text
+		.split_whitespace()
+		.filter(|str| str.contains(|c: char| c.is_alphabetic()))
+		.count()
+	}
+	fn count_phrases(text: &String) -> usize
+	{
+		text
+		.split(|c| c == '.' || c == '?' || c == '!')
 		.filter(|phrase| phrase.trim().is_empty() == false)
 		.count()
 	}
-	
-	fn word_len(text: String) -> usize
+	fn word_len(text: &String) -> usize
 	{
-		let word_count = Self::count_words(text.clone());
+		let word_count = Self::count_words(text);
 		let mut len: usize = 0;
-		let _ = text.split_whitespace().map(|word|len += word.len());
+		let _ = text
+		.split_whitespace()
+		.filter(|str| str.contains(|c: char| c.is_alphabetic()))
+		.map(|word| len += word.len());
 		return len / word_count;
 	}
-	fn most_used_words(text: String) -> Result<Option<String>, String>
+	fn most_used_word(text: &String) -> Option<Vec<String>>
 	{
+		let mut word_count = HashMap::new();
 		let words: Vec<String> = text
 			.split_whitespace()
 			.map(|tmp| tmp.to_string())
 			.collect();
-		let mut word_count = HashMap::new();
 	
 		if words.len() == 0 {
-			return Ok(None);
+			return None;
 		} for word in words {
 			word_count
 			.entry(word)
 			.and_modify(|count: &mut usize| *count += 1)
 			.or_insert(1);
 		}
+		let mut result = Vec::new();
+		let max = *word_count.values().max().unwrap();
 		for word in word_count.iter() {
-			if *word.1 == *word_count.values().max().unwrap() {
-				return Ok(Some((*word.0.clone()).to_string()));
+			if *word.1 == max {
+				result.push(word.0.clone());
 			}
 		}
-		Ok(None)
+		if result.is_empty() == true {
+			return None;
+		} else {
+			return Some(result);
+		}
 	}
 }
 
@@ -68,15 +92,21 @@ fn is_valid_path(path: &Path) -> bool {
 }
 
 fn main() {
-	let args: Vec<String> = env::args().collect();
-	if args.len() != 2 {
-		println!("Enter a file path")
+	let mut input: String = String::new();
+	let path;
+	println!("enter a file path:");
+	match std::io::stdin().read_line(&mut input) {
+		Ok(_) => {
+			path = Path::new(input.trim());	
+			if is_valid_path(path) == false {
+				return ;
+			}
+		} Err(_) => {
+			println!("bad input");
+			return ;
+		}
 	}
 
-	let path = Path::new(args[1].as_str());
-	if is_valid_path(path) == false {
-		return ;
-	}
     let file = match read_to_string(path) {
 		Ok(content) => content,
 		Err(_) => {
@@ -84,4 +114,29 @@ fn main() {
 			return ;
 		}
 	};
+
+	let most_used = Stats::most_used_word(&file).expect("Empty file");
+	
+	println!("There is {} characters", Stats::count_chars(&file));
+	println!("There is {} words", Stats::count_words(&file));
+	println!("The average word len is {}", Stats::word_len(&file));
+	if most_used.len() == 1 {
+		println!("The most used word is '{}'", most_used[0]);
+	} else {
+		print!("The most used word are");
+		let mut i: usize = 1;
+		for word in &most_used {
+			if i == 1 {
+				print!(" '{}'", *word);
+			} else if i == most_used.len() {
+				print!(" and '{}'", *word);
+			} else {
+				print!(", '{}'", *word);
+			}
+			i += 1;
+		}
+		println!("");
+	}
+	println!("There is {} phrases", Stats::count_phrases(&file));
+	println!("There is {} non-empty lines", Stats::count_lines(&file));
 }
